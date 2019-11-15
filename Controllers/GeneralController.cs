a854d092d0d5664a40139ac2e70e164fa074c32b
Samuel Coral Nuño <samisito56@hotmail.com>
2019-11-15 01:05:17 -0600

    Minor improvements.

--- Controllers/GeneralController.cs
+++ Controllers/GeneralController.cs
@@ -33,18 +33,21 @@ namespace SiteCauldron.Controllers
 
         public static Type GetEntityType(string entityType) =>
             Assembly.GetAssembly(typeof(Context)).GetTypes().FirstOrDefault(t =>
-                entityType.Equals(new Pluralizer().Pluralize(t.Name), StringComparison.OrdinalIgnoreCase)
+                entityType.Equals(
+                    new Pluralizer().Pluralize(t.Name),
+                    StringComparison.OrdinalIgnoreCase
+                )
             );
 
 
         public ActionResult Mutate(string entityType, object entity, string action) =>
-            GetEntityType(entityType).To(t => Ok(db.SaveAfter((EntityEntry)
+            GetEntityType(entityType)?.To(t => Ok(db.SaveAfter((EntityEntry)
                 typeof(Context).GetGenericMethod(action, t)
                 .Invoke(db, new object[] {
                     typeof(JsonSerializer).GetGenericMethod("Deserialize", t)
                     .Invoke(null, new object[] { entity.ToString(), null })
                 })
-            )));
+            ))) ?? (ActionResult)NotFound();
 
 
 
@@ -60,7 +63,8 @@ namespace SiteCauldron.Controllers
 
         [HttpGet("{entityType}/{id}")]
         public ActionResult Get(string entityType, int id) =>
-            GetEntityType(entityType)?.To(t => Ok(db.Find(t, id)));
+            GetEntityType(entityType)?.To(t => Ok(db.Find(t, id))) ??
+            (ActionResult)NotFound();
 
 
         [HttpPut("{entityType}")]
