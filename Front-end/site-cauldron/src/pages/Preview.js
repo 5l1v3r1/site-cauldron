64f0e1a669bee8e2cf74b161bbdd924472ae16f5
Leonel <falco-leonel@hotmail.com>
2019-11-25 16:20:32 -0600

    Preview progress 2

---
+++ Front-end/site-cauldron/src/pages/Preview.js
@@ -0,0 +1,15 @@
+import React,{component} from 'react';
+
+import '../styles/Preview.css'
+
+import Cauldron from '../images/Cauldron.jpg'
+
+function Preview() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Preview
